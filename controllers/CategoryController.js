673fe40a06af38c4e38d73c28a76ab24f8ea6f7c
omur8819 <omur8819@gmail.com>
2020-12-19 14:32:13 +0300

    Added addCategory function detail and other functions

--- controllers/CategoryController.js
+++ controllers/CategoryController.js
@@ -1,5 +1,43 @@
 const Category = require("../models/Category");
+const { validationResult } = require('express-validator');
 
 exports.addCategory = async (req, res) => {
     const { categoryName, description } = req.body;
+
+    //field validation
+    const validationErr = validationResult(req);
+    if(validationErr.errors.length > 0){
+        return res.status(400).json({errors: validationErr.array()});
+    }
+
+    //category exists check
+    const existCategory = await Category.findOne({categoryName: categoryName});
+    if(existCategory){
+        return res.status(400).json({errors: [{message: "Category already exists"}] });
+    }
+
+    //save category
+    const category = new Category({
+        categoryName: categoryName,
+        description: description
+    });
+
+    // const category = new Category(req.body);
+
+    const addedCategory = await category.save({new: true});
+    //res.status(200).send("Category added successfully");
+    res.status(200).json(addedCategory);
+}
+
+exports.getCategory = async (req, res) => {
+    
+}
+exports.updateCategory = async (req, res) => {
+
+}
+exports.deleteCategory = async (req, res) => {
+
+}
+exports.getCategories = async (req, res) => {
+
 }
---
+++ controllers/ProductController.js
@@ -0,0 +1,18 @@
+const Product = require("../models/Product");
+
+exports.addProduct = async (req, res) => {
+    const { ProductName, description } = req.body;
+}
+
+exports.getProduct = async (req, res) => {
+    
+}
+exports.updateProduct = async (req, res) => {
+
+}
+exports.deleteProduct = async (req, res) => {
+
+}
+exports.getProducts = async (req, res) => {
+
+}
